5d5ef447db06b7d9277a5a0784ea26ece4fa0930
Denny da Vjncj <daVjncjCode@gmail.com>
2020-11-30 21:10:28 -0500

    configuring api route

--- develop/routes/apiRoutes.js
+++ develop/routes/apiRoutes.js
@@ -1,20 +1,26 @@
 //this file will serve to create the pathway for data transers
-const userInput=require('../db/db.json')
+const userInput=require('../db/db.json');//stores notes
 const effThis=require('fs');
-const textArea=require('../js/index.js');
-
+const {v4:uuidv4}=require('uuid');//not called to action
+const { response } = require('express');//auto generated line
+const shiningPath=require('path');
+// let mineArray=[];//line 2 maybe fullfills this line
+const indexScript=require('../js/index.js');
 
 module.exports=xprssApp=>{
     xprssApp.get('/api/notes',(ask,echo)=>{
         echo.json(userInput);
     })
-    xprssApp.get('/api/*',(ask,echo)=>{
-        echo.json(userInput);
-    })
     xprssApp.post('/api/notes',(ask,echo)=>{
-        textArea.activenote.push()
+        echo.json(userInput);
+        let newNote=ask.body;
+        // console.log(newNote);
+        activeNote+=newNote;
+        let newId=uuidv4();
+        // console.log(newId);
+        userInput.push(activeNote);    
     })
-}
+}    
 
 //getNotes from dbJson file - complete!
     //getNote from ask.body which contains info from front end ajax call
