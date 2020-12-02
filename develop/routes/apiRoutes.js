93fba9080fd2b5453e6490bb28ddc62a94df6811
Denny da Vjncj <daVjncjCode@gmail.com>
2020-12-01 19:03:53 -0500

    still configuring POST rout

--- develop/routes/apiRoutes.js
+++ develop/routes/apiRoutes.js
@@ -1,26 +1,28 @@
 //this file will serve to create the pathway for data transers
 const userInput=require('../db/db.json');//stores notes
-const effThis=require('fs');//effThis.writeFile()-->this goes before the writefile method-->1st param is a string leading to our database, 2nd param is 
-const {v4:uuidv4}=require('uuid');//not called to action
-const { response } = require('express');//auto generated line
+const effThis=require('fs');//effThis.writeFile()-->this goes before the writefile method-->1st param is a string leading to our database, 2nd param is
+const {v4:uuidv4}=require('uuid');
 const shiningPath=require('path');
-// let mineArray=[];//line 2 maybe fullfills this line
+// let mineArray=[];//line 2 maybe fullfills this line, start seeking how I can make use thereof
 const until=require('util');
-const readFileAsync = until.promisify(effThis.readFile);
-const writeFileAsync = until.promisify(effThis.writeFile);
+// const readFileAsync = until.promisify(effThis.readFile); LEARNING ASSISTANT SUGGESTION
+// const writeFileAsync = until.promisify(effThis.writeFile); LA SUGGESTION
 
 module.exports=xprssApp=>{
     xprssApp.get('/api/notes',(ask,echo)=>{
         echo.json(userInput);
+        //if else condition that checks for error, if none, than I progrsss with 'effThis.writeFile('../db/db.json)'
     })
     xprssApp.post('/api/notes',(ask,echo)=>{
         echo.json(userInput);
         let newNote=ask.body;
         console.log(newNote);
         let newId=uuidv4();
-        console.log(newId);
+        // console.log(newId);
         newNote.newId=newId;
+        console.log(newId);
         userInput.push(newNote);
+        console.log(userInput);
 
         // JSON.stringify(userInput)-->this will be second param within the writeFile();        
         
