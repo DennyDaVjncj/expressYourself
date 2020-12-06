8905a4b7251d33aa7399dbe5e5af4454594d81a5
Denny da Vjncj <daVjncjCode@gmail.com>
2020-12-05 20:13:07 -0500

    version control

---
+++ vitamins/mod15.w11/data/waitingListData0.js
@@ -0,0 +1,9 @@
+let waitingArray=[
+    {
+        patronMoniker:'denny',
+        patronEmail:'davjncjC0d3@gmail.com',
+        phoneNumber:'407-732-8899'
+    }
+];
+
+module.exports=waitingArray;
