0480a23e218c47b958e9d0c426f1d8adab584d58
Denny da Vjncj <daVjncjCode@gmail.com>
2020-12-05 20:12:11 -0500

    final update

--- develop/routes/apiRoutes.js
+++ develop/routes/apiRoutes.js
@@ -1,37 +1,29 @@
 const userInput=require('../db/db.json');//stores notes
 const effThis=require('fs');
-const {v4:uuidv4}=require('uuid');
+const {v4:uuidv4}=require('uuid');//node pack
 const shiningPath=require('path');
-const until=require('util');
-const { Script } = require('vm');
-let id=uuidv4();
 
 module.exports=xprssApp=>{
     xprssApp.get('/api/notes',(ask,echo)=>{
-        effThis.readFile(shiningPath.join(__dirname,'../db/db.json'), (misfortune,reverb)=>{            
+        effThis.readFile(shiningPath.join(__dirname,'../db/db.json'),(misfortune,reverb)=>{            
             if(misfortune) throw misfortune;
             echo.json(JSON.parse(reverb));
         })
     })
     xprssApp.post('/api/notes',(ask,echo)=>{
-        echo.json(userInput);
-        let newNote=ask.body;        
-        newNote.id=id;
+        let newNote=ask.body;                      
+        newNote.id=uuidv4()
         userInput.push(newNote);        
         effThis.writeFileSync('./db/db.json', JSON.stringify(userInput));
+        echo.json(userInput);
     })
-    xprssApp.delete('/api/notes/:trash',(ask,echo)=>{
-        let trash=ask.params.trash;
-        console.log(trash);               
-        effThis.readFile(shiningPath.join(__dirname,'../db/db.json'),(misfortune,reverb)=>{            
-            if(misfortune) throw misfortune;
-            const noteBook=JSON.parse(reverb);
-            const pages=noteBook.filter(tornPage=>tornPage.trash)
-            const newBinder=tornPage.trash;
-            console.log(newBinder);
-            // Filter notes so that it removes the one matching id = recycable, save the filtered notes to a variable
-            // Write db.json again with new filtered notes.
-            // return res.json(true); 
-        })    
+    xprssApp.delete('/api/notes/:id',(ask,echo)=>{       
+            const diary=ask.params.id;            
+            const pages=userInput.filter(tornPage=>{
+                return tornPage.id!==diary;
+            })
+            console.log(pages);
+            effThis.writeFileSync('./db/db.json', JSON.stringify(pages))          
+            echo.json(pages);
     })
 }
